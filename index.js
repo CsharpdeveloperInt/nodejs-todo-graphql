5e6faabe23f670e54db15ecebb38cfca3009a359
oleg <csharpdeveloper@list.ru>
2020-08-10 12:10:03 +0600

    fix connection

--- index.js
+++ index.js
@@ -15,25 +15,16 @@ app.use((req,res,next)=>{
 
 //Запускаем сервер, и сервер БД
 async function start(){
-    try
-    {
 
       try {
         await sequelize.authenticate();
+        
         console.log('Connection has been established successfully.');
+        app.listen(PORT,()=>{
+          console.log(`Server is running on port ${PORT}`)});
       } catch (error) {
         console.error('Unable to connect to the database:', error);
       }
-
-      await sequelize.sync({ force: true }).then(()=>{
-        app.listen(PORT, function(){
-          console.log(`Server is running on port ${PORT}`);
-        });
-      }).catch(err=>console.log(err));
-    } catch(e)
-    {
-        console.log(e)
     }
-  }
-  
+
 start()
