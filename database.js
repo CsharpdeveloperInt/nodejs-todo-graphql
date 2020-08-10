7718cb73270e1f3afbfcdfe812418abbe6acd922
oleg <csharpdeveloper@list.ru>
2020-08-10 10:36:18 +0600

    fix sequelize mariadb

--- database.js
+++ database.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize')
 //Подключаем соединение с БД
 const sequelize = new Sequelize(config.MYSQL_DB,config.MYSQL_LOGIN,config.MYSQL_PASSWORD,{
     host: config.MYSQL_HOST,
-    dialect: 'mysql'
+    dialect: 'mariadb'
 })
 
 module.exports = sequelize
